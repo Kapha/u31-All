37168c2f082c9b3cffe792c48fb770bb51d72564
Peter Blomgren <blomgren-p@mssu.edu>
2012-11-04 14:46:43 -0600

    Beta phase signup page

--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,11 @@ SampleApp::Application.routes.draw do
   match '/main',    to: "static_pages#home"
   match '/help',    to: "static_pages#help"
   match '/about',   to: "static_pages#about"
-  match '/signup',  to: 'users#new'
+  match '/signup',  to: 'static_pages#beta'
   match '/contact', to: "static_pages#contact"
   match '/signin', to: 'sessions#new'
   match '/signout', to: "sessions#destroy", via: :delete
+  match '/backdoor', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
