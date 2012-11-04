eb913cfcda88cb8d39d8ff38e1eabe0f252d675e
Peter Blomgren <blomgren-p@mssu.edu>
2012-11-03 23:51:01 -0500

    Added rating system to reviews and average stats to book and author pages

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ActiveRecord::Base
   attr_accessible :a1stname, :asurname
   has_many :books
+  has_many :reviews, :through => :books
   def afull_name
     "#{asurname}, #{a1stname}"
   end
